166d90dd695ab797c8eef15666d4959968176fed
Kalinin Vladislav <Kalinavl1995@gmail.com>
2024-06-05 17:06:16 +0300

    feature(cabins/page.js) add fetching and displaying cabin list

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,14 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "@/app/_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
-  // CHANGE
-  const cabins = [];
+export default async function Page() {
+  console.log("loading..")
+  const cabins = await getCabins();
+  console.log(cabins)
 
   return (
     <div>
